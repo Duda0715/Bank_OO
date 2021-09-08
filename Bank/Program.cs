25dfde42b7286263d62bcdacd0230a2e959cb9cb
Guilherme <silveiramaria.eduarda17@gmail.com>
2021-09-08 09:52:23 -0300

    Adicionei a conta corrente

---
+++ Bank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaDuda = new ContaCorrente();
+
+           contaDaDuda.titular = "Duda";
+           contaDaDuda.agencia = 1234;
+           contaDaDuda.conta = 789;
+           contaDaDuda.saldo = 100.50;
+
+           ContaCorrente contaDoLucas = new ContaCorrente();
+           contaDoLucas.titular = "Lucas";
+           contaDoLucas.agencia = 4321;
+           contaDoLucas.conta = 987;
+           contaDoLucas.saldo = 100.50;
+
+           Console.WriteLine("O titular da conta é:" + contaDaDuda.titular);
+           Console.WriteLine("O titular da conta é:" + contaDoLucas.titular);
+        }
+    }
+}
